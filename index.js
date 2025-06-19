061726c0f637462b2b0f0136c26751510bcdf59f
Mercy5-k <kiqsmercy@gmail.com>
2025-06-19 15:27:16 +0300

    All functions implemented and tests passing

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1. Function that receives and calls a callback
+function receivesAFunction(callback) {
+  callback();
+}
+
+// 2. Function that returns a named function
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("I am a named function");
+  };
+}
+
+// 3. Function that returns an anonymous function
+function returnsAnAnonymousFunction() {
+  return () => console.log("I am an anonymous function");
+}
